eb226e1567bc039478ebd2b7eb386992e4f1807e
Fernando Farji <fernandounlu@hotmail.com>
2026-03-26 16:45:26 -0300

    nuevo

---
+++ 01-trabajoPractico-Recursividad/Ejercicio01/maintp01ej01.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+// Ejercicio 1
+/*1.	Construir una función recursiva que retorne verdadero si una cadena de caracteres pasada 
+como parámetro es un palíndromo.  Ej. “neuquen” ya que se lee igual de atrás hacia adelante.*/
+int main (){
+    printf("hollaaaaa main\n");
+    system("pause");
+
+   }
---
+++ 01-trabajoPractico-Recursividad/Ejercicio01/resolucionEj01.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+
+
+
+// Ejercicio 1
+/*1.	Construir una función recursiva que retorne verdadero si una cadena de caracteres pasada 
+como parámetro es un palíndromo.  Ej. “neuquen” ya que se lee igual de atrás hacia adelante.*/
+bool palindromo (char * cadena){
+    printf("hollaaaaa");
+    return true;
+}
