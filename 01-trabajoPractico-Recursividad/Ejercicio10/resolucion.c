5b89f95fbcdc6ae8b8199c507f91e6c884facea9
Fernando Farji <fernandounlu@hotmail.com>
2026-03-26 18:55:17 -0300

    nuevo

---
+++ 01-trabajoPractico-Recursividad/Ejercicio10/resolucion.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+// Ejercicio 1
+/*1.	Construir una función recursiva que retorne verdadero si una cadena de caracteres pasada 
+como parámetro es un palíndromo.  Ej. “neuquen” ya que se lee igual de atrás hacia adelante.*/
+bool palindromo (char * cadena){
+    printf("hollaaaaa");
+    return true;
+}
