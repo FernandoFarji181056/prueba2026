e2ec8cc669764e7be25928275233930ee26b3e31
Fernando Farji <fernandounlu@hotmail.com>
2026-03-29 11:09:03 -0300

    bomba

--- 01-trabajoPractico-Recursividad/Ejercicio10/maintp01ej10.c
+++ 01-trabajoPractico-Recursividad/Ejercicio10/maintp01ej10.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
+#include <limits.h>
 #include "../../libs/tp0repaso/headers/TP0_Repaso.h"
 #include "../../libs/recursividad/headers/TP_1_recursividad.h"
 
 #include "../../libs/validaciones/headers/Validacion_datos.h"
 
 //C:\Estructura2026\Repositorios\prueba2026\libs\tp0repaso\headers
-
-
+//-2.147.483.648
+// 2.147.483.647
 int main() {
+
 
     char ingresoNumero[100];
     int numero;
@@ -17,18 +19,27 @@ int main() {
     bool validar=false;
 
     do{
-    printf("ingrese un numero entero menor a 1000000: ");
+    printf("ingrese un numero entero mayor a 0 y menor a 100000: ");
     fgets(ingresoNumero, 100, stdin);
     ingresoNumero[strcspn(ingresoNumero, "\n")] = 0;
     validar=validarEntero(ingresoNumero);
 
     if (validar){
-        printf("es un entero valido\n");
-        numero = strtol(ingresoNumero,NULL,10);    
+
+        numero = strtol(ingresoNumero,NULL,10);
+
+        if (numero <1 || numero > 99999){
+        printf("el numero esta fuera del rango permitido\n");
+        printf("intentelo nuevamente\n");
+        validar=false;
+            }  
+
         }
     else{printf("NOO es un entero valido, intentelo nuevamente\n");}
     
-    printf("Usted ingreso: %s\n",ingresoNumero);
+    //printf("Usted ingreso: %s\n",ingresoNumero);
+
+
         
     }while (!validar);
 
@@ -39,13 +50,23 @@ int main() {
     clearScreen();
 
     do{
-    printf("ingrese bomba: ");
+    printf("ingrese bomba mayor a 1 y menor al numero anteriormente ingresado: ");
     fgets(ingresoNumero, 100, stdin);
     ingresoNumero[strcspn(ingresoNumero, "\n")] = 0;
     validar=validarEntero(ingresoNumero);
 
-    if (validar){printf("es un entero valido\n");
+    if (validar){
+            //printf("es un entero valido\n");
             bomba = strtol(ingresoNumero,NULL,10); 
+                 if (bomba <2 || bomba > 99999 || numero<=bomba){
+                    printf("\nla bomba esta fuera del rango permitido\n");
+                    printf("o la bomba no es menor al numero\n");
+                    printf("intentelo nuevamente\n\n");
+                    validar=false;
+            }  
+
+
+
              }
 
             else{
